bd5521de45f2be6ea20c1d8210b00eba1c586f6d
Erik477 <57523505+Erik477@users.noreply.github.com>
2022-11-11 11:10:24 +0100

    final

--- Pokerspielsimulator.py
+++ Pokerspielsimulator.py
@@ -1,16 +1,3 @@
-#Karten modellieren: 52 Karten-> Array mit 52 Stellen -> zufällige Zahl ziehen 0 bis 52 -> 
-# Bei Farbe modulo 4 => 1 bis 4 kommt da raus ->
-# Bei Symbolen dividieren durch 13 ->
-
-# 5 karten ziehen und in eine Liste packen und dann überprüfen ob eine der Kombinationen Zutrifft
-
-# Wahrscheinlichkeiten für Kombinationen nach 1000 Versuchen Ausgeben
-
-#https://de.wikipedia.org/wiki/Poker
-
-
-#TODO: Bei 7 Karten Funktioniert die Straße nicht so wie sie sollte
-
 import random
 
 def Kartenziehung(anzZiehungen,anzKarten):
@@ -19,10 +6,10 @@ def Kartenziehung(anzZiehungen,anzKarten):
         ziehungen.append(x)
     for x in range(0, anzZiehungen):
         index = random.randrange(len(ziehungen) - x)
-
-        last_pos = len(ziehungen) -1 - x    #Herausfinden der letzten noch nicht besetzten stelle
-        ziehungen[last_pos],ziehungen[index]=ziehungen[index],ziehungen[last_pos] #Zufallszahl und letzte noch nicht besetzte stelle werden getauscht
-
+        last_pos = len(ziehungen) -1 - x    
+        #Herausfinden der letzten noch nicht besetzten stelle
+        ziehungen[last_pos],ziehungen[index]=ziehungen[index],ziehungen[last_pos] 
+        #Zufallszahl und letzte noch nicht besetzte stelle werden getauscht
     return ziehungen[-anzZiehungen:]
     
 def Kombinationen(Karten,aceistheworst):
@@ -39,19 +26,13 @@ def Kombinationen(Karten,aceistheworst):
             kombination = "Paar"
         elif ident == 3:                    #Drilling
             kombination = "Drilling"
-        elif ident == 4:                    #Vierling
+        elif ident == 6:                    #Vierling
             kombination = "Vierling"
-        elif ident == 22:                   #Zwei Paar
+        elif ident == 4:                   #Zwei Paar
             kombination = "2 Paare"
-        elif ident == 222:
-            kombination = "mehr als 2 Paare"
-        elif ident == 32:                   #Full House     (ein Paar ein Drilling)
+        elif ident == 5:                   #Full House     (ein Paar ein Drilling)
             kombination = "Full House"
-        elif ident == 42:
-            kombination = "vierling und ein zwilling"
-        elif ident == 5:
-            kombination = "mehr als 4 Gleiche"
-        elif StraightFlush(Karten,aceistheworst):         #Straße         
+        if StraightFlush(Karten,aceistheworst): #Straße         
             kombination = "Straße" 
 
     return(kombination)
@@ -82,9 +63,9 @@ def Flush(Karten):
 def StraightFlush(Karten,aceistheworst):
     Numbers = []
     for x in range(len(Karten)):
-        if aceistheworst == False:
-            if Karten[x][1] == 1:
-                Karten[x][1] = 14
+        #if aceistheworst == False:
+            #if Karten[x][1] == 1:
+                #Karten[x][1] = 14
         Numbers.append(Karten[x][1])
     Numbers.sort()
     if len(Numbers) == 5:
@@ -119,30 +100,21 @@ def RoyalFlush(Karten):
 
 def Gleiche(Karten):
     result = 0
-    counting = []
-    for x in range(14):
-        counting.append(0)
+    counting = [0] * 15
+
     for x in range(len(Karten)):
         counting[Karten[x][1]] += 1
+       
     if counting.count(2) > 0:
         result = 2
         if counting.count(2) == 2:
-            result = 22 
-        elif counting.count(2) > 2:
-            result = 222   
-        if counting.count(3) > 0:
-            result = 32 
-        if counting.count(4) > 0:
-            result = 42         
+            result = 22                  
     elif counting.count(3) > 0:
         result = 3
     elif counting.count(4) > 0:
         result = 4
-
-    for x in range(len(counting)):
-        if counting[x] > 4:
-            result = 5
-    
+   
+   
     return result
 
 def Farbe(nr):
@@ -193,22 +165,15 @@ def Aufrufen(anzZiehungen,aceistheworst,anzKartenimdeck):
         Karten.append(Karte)
         Karte = []
     
-
     erg = Kombinationen(Karten,aceistheworst)
-
-    #print("Ziehung: " + str(Ziehung))
-    #print("Karte1: " + Farbe(Karten[0][0]) + " " + Zahl(Karten[0][1]))
-    #print("Karte2: " + Farbe(Karten[1][0]) + " " + Zahl(Karten[1][1]))
-    #print("Karte3: " + Farbe(Karten[2][0]) + " " + Zahl(Karten[2][1]))
-    #print("Karte4: " + Farbe(Karten[3][0]) + " " + Zahl(Karten[3][1]))
-    #print("Karte5: " + Farbe(Karten[4][0]) + " " + Zahl(Karten[4][1]))
-    #print("Ergebnis: " + erg)
 
     return erg
 
 def Statistik(wieoft,anzKarten,aceistheworst,anzKartenimdeck):
 
-    ErgList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ErgList = []
+    for x in range(0, 14):
+        ErgList.append(0)
 
     for wieoft in range(0, wieoft):
         
@@ -225,10 +190,15 @@ def Statistik(wieoft,anzKarten,aceistheworst,anzKartenimdeck):
         elif Ergebnis == "mehr als 2 Paare": ErgList[9] += 1
         elif Ergebnis == "Full House":ErgList[10] += 1
         elif Ergebnis == "vierling und ein zwilling": ErgList[11] += 1
-        elif Ergebnis == "mehr als 4 Gleiche": ErgList[12] += 1
-        elif Ergebnis == "Straße": ErgList[13] += 1
-    
+        elif Ergebnis == "Straße": ErgList[13] += 1            
+
+    print("------------------------------------------------------")
+    print("Anzahl gezogener Karten: " + str(anzKarten))
+    print("Ist Ass die schlechteste Karte? " + str(aceistheworst))
+    print("Anzahl der Karten im deck: " + str(anzKartenimdeck))
+    print()
     print("Wahrscheinlichkeiten:")
+    print()
 
     for x in range(1,len(ErgList)):
         percent = (ErgList[x]/ErgList[0])*100
@@ -243,16 +213,14 @@ def Statistik(wieoft,anzKarten,aceistheworst,anzKartenimdeck):
         elif x == 7: print("Vierling: " + str(percent) + "%")
         elif x == 3: print("Straight Flush: " + str(percent) + "%")
         elif x == 4: print("Royal Flush: " + str(percent) + "%")        
-        #elif x == 9: print("mehr als 2 Paare: " + str(percent) + "%")
-        #elif x == 11: print("Vierling und ein Zwilling: " + str(percent) + "%")
-        #elif x == 12: print("mehr als 4 Gleiche: " + str(percent) + "%")    
+        elif x == 9: print("mehr als 2 Paare: " + str(percent) + "%")
+        elif x == 11: print("Vierling und ein Zwilling: " + str(percent) + "%") 
 
 if __name__ == '__main__':
-    stat = {}
 
     aceistheworst  = False
-    anzKarten = 7
+    anzKarten = 5
     wieoft = 100000
+    
     anzKartenimdeck = 52
     Statistik(wieoft,anzKarten,aceistheworst,anzKartenimdeck)
-
