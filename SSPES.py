05a9f099c7f7a74a090a27269d283b5fc835a8cd
Erik477 <57523505+Erik477@users.noreply.github.com>
2022-12-10 12:58:58 +0100

    Statistik Menü und comp verbessert

--- SSPES.py
+++ SSPES.py
@@ -1,13 +1,34 @@
 import random
+import json
 
 
-def aufrufen(Countplayerwins,Countcompwins):
+def aufrufen(Countplayerwins,Countcompwins,symbols_player,symbols_comp):
     print("Welcome to Schere Stein Papier Echse Spock")
+    print("Schwierigkeit: 1 = leicht, 2 = schwer")
+    difficulty = str(input())
+
+
     print("Schere = 0, Stein = 1, Papier = 2, Echse = 3, Spock = 4")
     print("")
-    comp = str(random.randint(0,4))
+    if difficulty == "1": 
+        comp = str(random.randint(0,4))
+    elif difficulty == "2":
+        comp = str(random.randint(0,4))
+        if comp == "0" and symbols_player[4] > symbols_player[2]:
+            comp = "4"
+        elif comp == "1" and symbols_player[0] > symbols_player[3]:
+            comp = "0"
+        elif comp == "2" and symbols_player[1] > symbols_player[4]:
+            comp = "1"
+        elif comp == "3" and symbols_player[2] > symbols_player[1]:
+            comp = "2"
+        elif comp == "4" and symbols_player[3] > symbols_player[0]:
+            comp = "3"
+    else: print("Falsche Eingabe bei Schwireigkeit")
     player = str(input())
 
+    symbols_player[int(player)] += 1
+    symbols_comp[int(comp)] += 1
 
     print("Computer: " + str(comp))
 
@@ -31,18 +52,119 @@ def aufrufen(Countplayerwins,Countcompwins):
 
     again = str(input("Nochmal? (y/n)"))
 
-    if again == "y": aufrufen(Countplayerwins,Countcompwins)
+    if again == "y": aufrufen(Countplayerwins,Countcompwins,symbols_player,symbols_comp)
     elif again == "n":
-        timesplayed = Countplayerwins + Countcompwins
-        print("Gespielt: " + str(timesplayed))
-        print("Spieler:  " + str(Countplayerwins) + " Percent: " + str((Countplayerwins/timesplayed)*100)+ "%")
-        print("Computer: " + str(Countcompwins) + " Percent: " + str((Countcompwins/timesplayed)*100) + "%")
         
+        playerstats = {"Schere": symbols_player[0], "Stein": symbols_player[1], "Papier": symbols_player[2], "Echse": symbols_player[3], "Spock": symbols_player[4], "Gewonnen": Countplayerwins}
+        compstats = {"Schere": symbols_comp[0], "Stein": symbols_comp[1], "Papier": symbols_comp[2], "Echse": symbols_comp[3], "Spock": symbols_comp[4], "Gewonnen": Countcompwins}
+
+        with open('playerstats.json', 'w') as f:
+            json.dump(playerstats, f)
+        with open('compstats.json', 'w') as f:
+            json.dump(compstats, f)
+
         print("Auf Wiedersehen")
     else: print("Falsche Eingabe")
 
+def statistic( Countplayerwins, Countcompwins, symbols_player, symbols_comp):
+    timesplayed = Countplayerwins + Countcompwins
+    print("Gespielt: " + str(timesplayed))
+    print("Spieler:  " + str(Countplayerwins) + " Percent: " + str((Countplayerwins/timesplayed)*100)+ "%")
+    print("Symbole: " + str(symbols_player))
+    print("Computer: " + str(Countcompwins) + " Percent: " + str((Countcompwins/timesplayed)*100) + "%")
+    print("Symbole: " + str(symbols_comp))
+
+def uploaddata(Countplayerwins,Countcompwins,symbols_player,symbols_comp):
+    playerstats = {"Schere": symbols_player[0], "Stein": symbols_player[1], "Papier": symbols_player[2], "Echse": symbols_player[3], "Spock": symbols_player[4], "Gewonnen": Countplayerwins}
+    compstats = {"Schere": symbols_comp[0], "Stein": symbols_comp[1], "Papier": symbols_comp[2], "Echse": symbols_comp[3], "Spock": symbols_comp[4], "Gewonnen": Countcompwins}
+
+    print("Playerstats: " + str(playerstats))
+    print("Compstats: " + str(compstats))
+
+    changedata = str(input("Daten ändern? (y/n)"))
+
+    if changedata == "y":
+        print("Welche Daten sollen geändert werden?")
+        print("1. Spieler")
+        print("2. Computer")
+        print("3. Beide")
+        whatdata = str(input())
+
+        if whatdata == "1" or whatdata == "3":
+            print("Wie viele Spiele hat der Spieler gewonnen?")
+            Countplayerwins = int(input())
+            print("Wie viele Schere hat der Spieler geworfen?")
+            symbols_player[0] = int(input())
+            print("Wie viele Stein hat der Spieler geworfen?")
+            symbols_player[1] = int(input())
+            print("Wie viele Papier hat der Spieler geworfen?")
+            symbols_player[2] = int(input())
+            print("Wie viele Echse hat der Spieler geworfen?")
+            symbols_player[3] = int(input())
+            print("Wie viele Spock hat der Spieler geworfen?")
+            symbols_player[4] = int(input())
+            print("Daten geändert")
+            if whatdata == "3":
+                print("Wie viele Spiele hat der Computer gewonnen?")
+                Countcompwins = int(input())
+                print("Wie viele Schere hat der Computer geworfen?")
+                symbols_comp[0] = int(input())
+                print("Wie viele Stein hat der Computer geworfen?")
+                symbols_comp[1] = int(input())
+                print("Wie viele Papier hat der Computer geworfen?")
+                symbols_comp[2] = int(input())
+                print("Wie viele Echse hat der Computer geworfen?")
+                symbols_comp[3] = int(input())
+                print("Wie viele Spock hat der Computer geworfen?")
+                symbols_comp[4] = int(input())
+                print("Daten geändert")
+        elif whatdata == "2":
+            print("Wie viele Spiele hat der Computer gewonnen?")
+            Countcompwins = int(input())
+            print("Wie viele Schere hat der Computer geworfen?")
+            symbols_comp[0] = int(input())
+            print("Wie viele Stein hat der Computer geworfen?")
+            symbols_comp[1] = int(input())
+            print("Wie viele Papier hat der Computer geworfen?")
+            symbols_comp[2] = int(input())
+            print("Wie viele Echse hat der Computer geworfen?")
+            symbols_comp[3] = int(input())
+            print("Wie viele Spock hat der Computer geworfen?")
+            symbols_comp[4] = int(input())
+            print("Daten geändert")
+        else: print("Falsche Eingabe")
+
+        with open('playerstats.json', 'w') as f:
+            json.dump(playerstats, f)
+        with open('compstats.json', 'w') as f:
+            json.dump(compstats, f)
 
 if __name__ == '__main__':
-    Countplayerwins = 0
-    Countcompwins = 0
-    aufrufen(Countplayerwins,Countcompwins)
+
+    with open('playerstats.json', 'r') as f:
+        playerstats = json.load(f)
+
+    with open('compstats.json', 'r') as f:
+        compstats = json.load(f)
+
+    Countplayerwins = playerstats["Gewonnen"]
+    Countcompwins = compstats["Gewonnen"]
+    symbols_player = [playerstats["Schere"],playerstats["Stein"],playerstats["Papier"],playerstats["Echse"],playerstats["Spock"]]
+    symbols_comp = [compstats["Schere"],compstats["Stein"],compstats["Papier"],compstats["Echse"],compstats["Spock"]]
+
+    print("Willkommen bei Schere Stein Papier Echse Spock")
+    print("1. Spiel starten")
+    print("2. Statistik")
+    print("3. Daten ändern")
+    print("4. Beenden")
+    choice = str(input())
+    if choice == "1":
+        aufrufen(Countplayerwins,Countcompwins,symbols_player,symbols_comp)
+    elif choice == "2":
+        statistic( Countplayerwins, Countcompwins, symbols_player, symbols_comp)
+    elif choice == "3":
+        uploaddata(Countplayerwins,Countcompwins,symbols_player,symbols_comp)
+    elif choice == "4":
+        print("Auf Wiedersehen")
+        exit()
+    else: print("Falsche Eingabe")
